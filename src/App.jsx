5dd5fa762411db9652ce948991d7755418a2565a
Davids-Adamovics <davidsadamovicss@gmail.com>
2024-06-14 21:25:12 +0300

    rest

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,40 @@
-import { useState } from 'react'
-import './App.css'
+import { Suspense } from "react";
+import "./App.css";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+import Model from "../public/Realshake";
+import Model2 from "../public/Scene";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-     
+    <h1>3D models & JSX</h1>
+
+      <div className="canvas-wrapper">
+        <div className="canvas-container">
+          <Canvas camera={{ position: [0, 0, 10] }}>
+            <ambientLight />
+            <OrbitControls enablePan={false} />
+            <Suspense fallback={null}>
+              <Model position={[0, 0, 0]} />
+            </Suspense>
+          </Canvas>
+          <h1 className="model-title">Berry shake</h1>
+        </div>
+
+        <div className="canvas-container">
+          <Canvas camera={{ position: [0, 0, 10] }}>
+            <ambientLight />
+            <OrbitControls enablePan={false} />
+            <Suspense fallback={null}>
+              <Model2 position={[0, 0, 0]} />
+            </Suspense>
+          </Canvas>
+          <h1 className="model-title">Dark spongebob</h1>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
